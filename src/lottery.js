c61345f01d4d1e2e33184bb8c1a8e2e68a687413
ggroce <ggrocedev@gmail.com>
2021-06-07 19:45:23 -0500

    contract address change

--- src/lottery.js
+++ src/lottery.js
@@ -1,6 +1,6 @@
 import web3 from './web3';
 
-const contractAddress = '0x67000421dA503ECBb24DCA1e81B435e65d06aDa6';
+const contractAddress = '0xC1619920bBF11e6eC9D6D5291882Ad9157C50Cf5';
 const contractABI = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
 export default new web3.eth.Contract(contractABI, contractAddress);
