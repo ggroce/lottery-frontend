6c46b055b4ab9fe93ac4882538ca057fbad71ad4
ggroce <ggrocedev@gmail.com>
2021-06-07 19:42:28 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import web3 from './web3.js';
+import lottery from './lottery.js';
+
+class App extends React.Component {
+  state = {
+    manager: '', 
+    players: [], 
+    balance: '', 
+    value: '', 
+    message: '',
+    test1: 'testthis', 
+    test2: 'testthis',
+  };
+
+  async componentDidMount() {
+    const manager = (await lottery.methods.manager().call()).toLowerCase();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    // console.log(manager);
+    this.setState({ manager, players, balance });
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+    if (!this.state.value || this.state.value === "") {
+      return;
+    }
+
+    // const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction...' });
+
+    await lottery.methods.enter().send({ 
+      from: window.ethereum.selectedAddress, 
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({ message: 'Your entry is complete!' });
+  }
+
+  onClick = async () => {
+    if (this.players <= 0) {
+      return;
+    }
+
+    this.setState({ message: 'Picking a winner...' });
+
+    await lottery.methods.pickWinner().send({ 
+      from: window.ethereum.selectedAddress
+    });
+
+    this.setState({ message: 'Winner selected!' });
+  }
+
+  isManager = () => {
+    if (window.ethereum.selectedAddress === this.state.manager) {
+      return(
+        <div>
+        <hr />
+        <h4>Pick a winner?</h4>
+        <button onClick={this.onClick}>Award a lotto winner!</button>
+        </div>
+      );
+    } 
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contact is managed by {this.state.manager}</p>
+        <p>There are currently {this.state.players.length} players entered. </p>
+        <p>Players available are: {this.state.players}</p>
+        <p>Total pot contains: {web3.utils.fromWei(this.state.balance, 'ether')} eth</p>
+
+        <form onSubmit={this.onSubmit}>
+          <h2>Enter lotto?</h2>
+          <div>
+            <label>Amount of ETH to bet: </label>
+            <input 
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })} />
+          </div>
+          <button>Enter</button>
+        </form>
+
+        {this.isManager()}
+
+        <hr />
+        <p style={{fontStyle: 'italic'}}>{this.state.message}</p>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/lottery.js
@@ -0,0 +1,6 @@
+import web3 from './web3';
+
+const contractAddress = '0x67000421dA503ECBb24DCA1e81B435e65d06aDa6';
+const contractABI = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(contractABI, contractAddress);
